fbe75f438dc1bf88f8d53a21d738ae7d7d04b3c7
LucassSantoss <lucascross.pereirasantos@gmail.com>
2025-02-21 08:46:37 -0300

    feat: add a number field to edit initial value to counter

--- app/src/main/java/com/lucas/contador/MainActivity.kt
+++ app/src/main/java/com/lucas/contador/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.lucas.contador
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
     private lateinit var cliqueBt: Button
     private lateinit var contadorTv: TextView
+    private lateinit var inicialEt: EditText
     private var contador = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +24,26 @@ class MainActivity : AppCompatActivity() {
 
         cliqueBt = findViewById(R.id.cliqueBt)
         contadorTv = findViewById(R.id.contadorTv)
+        inicialEt = findViewById(R.id.inicial_et)
+
+        /*
+        inicialEt.addTextChangedListener(object: TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // NSA
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                // NSA
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+        })
+        */
+
+        inicialEt.addTextChangedListener { contador = it.toString().toInt() }
 
         cliqueBt.setOnClickListener {
             contadorTv.text = (++contador).toString()
